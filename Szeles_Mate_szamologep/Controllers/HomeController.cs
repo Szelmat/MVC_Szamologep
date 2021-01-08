c4bc121580788a335ec23b78d600b583a2af6b44
Szelmat <szelessm@gmail.com>
2021-01-08 12:30:45 +0100

    failsafe improvements

--- Szeles_Mate_szamologep/Controllers/HomeController.cs
+++ Szeles_Mate_szamologep/Controllers/HomeController.cs
@@ -14,6 +14,8 @@ namespace Szeles_Mate_szamologep.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        String[] ops = { "+", "-", "*", "×", "/" };
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -32,31 +34,55 @@ namespace Szeles_Mate_szamologep.Controllers
         [HttpPost]
         public IActionResult Equals()
         {
+            // Beírt adatok lekérése
             string firstNum = Request.Form["firstTypingRow"];
             string secondNum = Request.Form["secondTypingRow"];
 
+            // Ha valamelyik számot nem adtuk meg, akkor az 0
             if (firstNum.Equals(""))
                 firstNum = "0";
 
             if (secondNum.Equals(""))
                 secondNum = "0";
 
+            // A műveleti jel és a szám elválasztása a space alapján
             string[] firstNumParts = firstNum.Split(" ");
+            string operation = "";
+            if (firstNumParts.Length < 2)
+            {
+                foreach(string op in ops) {
+                    // Nem választja el space a műveleti jelet
+                    if (firstNum.IndexOf(op) != -1)
+                    {
+                        operation = op;
+                    }
 
-            string operation;
-            try
+                    // Nem adtunk meg műveletet
+                    if(operation == "")
+                    {
+                        operation = "+";
+                    }
+                }
+            } 
+            else
             {
-                operation = firstNumParts[1];
-            } catch (IndexOutOfRangeException)
-            {
-                operation = "+";
+                try
+                {
+                    operation = firstNumParts[1];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    operation = "+";
+                }
             }
-            
 
+            // Csak a számjegyeket és a tizedesvesszőket fogadjuk el
             double a = Convert.ToDouble(Regex.Replace(firstNumParts[0], "[^0-9,]", ""));
             double b = Convert.ToDouble(Regex.Replace(secondNum, "[^0-9,]", ""));
+            
             double result = 0;
 
+            // A művelet elvégzése műveleti jel alapján
             if (operation == "+")
             {
                 result = a + b;
@@ -73,6 +99,8 @@ namespace Szeles_Mate_szamologep.Controllers
             {
                 result = a / b;
             }
+
+
             ViewData["Result"] = result;
             return View("Index");
         }
